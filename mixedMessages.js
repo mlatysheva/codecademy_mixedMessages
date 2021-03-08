56ab42d87cbe70446d55ae67dda59af36aa951e2
Maria-Latysheva <mlatysheva@gmail.com>
2021-03-08 08:48:03 +0100

    Function wrap up

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,26 +1,31 @@
 // Terminal javascript application with random inspirational messages for students.
 
-const lifeArray = ['easy', 'simple', 'exiting', 'dissappointing'];
-const studyingArray = ['effort', 'time', 'thought', 'dedication'];
-const sayingsObjArray = [{author: 'Vincent Van Gogh', saying: 'If you hear a voice within you say ‘you cannot paint,’ then by all means paint, and that voice will be silenced.'},
-    {author: 'Mark Twain', saying: 'Don’t go around saying the world owes you a living. The world owes you nothing. It was here first.'},
-    {author: 'Winston Churchill', saying: 'Success consists of going from failure to failure without loss of enthusiasm.'},
-    {author: 'Alexander Graham Bell', saying: 'When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one which has opened for us.'},
-    {author: 'Thomas A. Edison', saying: 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.'},
-    {author: 'Nora Roberts', saying: 'If you don’t go after what you want, you’ll never have it. If you don’t ask, the answer is always no. If you don’t step forward, you’re always in the same place.'},
-    {author: 'C.S. Lewis', saying: 'There are far, far better things ahead than any we leave behind.'}];
+function mixedMessages() {
+    const lifeArray = ['easy', 'simple', 'exiting', 'dissappointing'];
+    const studyingArray = ['effort', 'time', 'thought', 'dedication'];
+    const sayingsObjArray = [{author: 'Vincent Van Gogh', saying: 'If you hear a voice within you say ‘you cannot paint,’ then by all means paint, and that voice will be silenced.'},
+        {author: 'Mark Twain', saying: 'Don’t go around saying the world owes you a living. The world owes you nothing. It was here first.'},
+        {author: 'Winston Churchill', saying: 'Success consists of going from failure to failure without loss of enthusiasm.'},
+        {author: 'Alexander Graham Bell', saying: 'When one door closes, another opens; but we often look so long and so regretfully upon the closed door that we do not see the one which has opened for us.'},
+        {author: 'Thomas A. Edison', saying: 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.'},
+        {author: 'Nora Roberts', saying: 'If you don’t go after what you want, you’ll never have it. If you don’t ask, the answer is always no. If you don’t step forward, you’re always in the same place.'},
+        {author: 'C.S. Lewis', saying: 'There are far, far better things ahead than any we leave behind.'}];
 
-const lenLife = lifeArray.length;
-const lenStudying = studyingArray.length;
-const lenSayings = sayingsObjArray.length;
+    const lenLife = lifeArray.length;
+    const lenStudying = studyingArray.length;
+    const lenSayings = sayingsObjArray.length;
 
-function createMessage() {
-    const lifeMessage = `Life is not always ${lifeArray[Math.floor(Math.random() * lenLife)]}.`;
-    const studyingMessage = `Studying requires ${studyingArray[Math.floor(Math.random() * lenStudying)]}.`;
-    const ind = Math.floor(Math.random() * lenSayings);
-    const sayingMessage = `As ${sayingsObjArray[ind]['author']} once said, "${sayingsObjArray[ind]['saying']}"`;
-    const finalMessage = `${lifeMessage} \n${studyingMessage} \n${sayingMessage}`;
-    
-    return finalMessage;
+    function createMessage() {
+        const lifeMessage = `Life is not always ${lifeArray[Math.floor(Math.random() * lenLife)]}.`;
+        const studyingMessage = `Studying requires ${studyingArray[Math.floor(Math.random() * lenStudying)]}.`;
+        const ind = Math.floor(Math.random() * lenSayings);
+        const sayingMessage = `As ${sayingsObjArray[ind]['author']} once said, "${sayingsObjArray[ind]['saying']}"`;
+        const finalMessage = `${lifeMessage} \n${studyingMessage} \n${sayingMessage}`;
+        
+        return finalMessage;
+    }
+    const message = createMessage();
+    return message;
 }
-console.log(createMessage());
+
+console.log(mixedMessages());
